af3798f0ab078bb583d65b9d4bc7822448f4d566
stefancernescu <stefan.cernescu94@gmail.com>
2020-01-12 11:57:27 +0200

    added paterns in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,35 @@ import proxy.RealSubject;
 public class Main {
 
     public static void main(String[] args) {
+
+        //Proxy
+        proxy.Subject realSubject = new RealSubject();
+        proxy.Subject proxy = new Proxy(realSubject);
+        System.out.println(proxy.doACtion());
+        System.out.println(realSubject.doACtion());
+
+        //Observer
+        Subject subject = new SubjectImpl();
+        Observer observer1 = new ObserverImpl();
+        Observer observer2 = new ObserverImpl();
+        Observer observer3 = new ObserverImpl();
+        observer1.subscribe(subject);
+        observer2.subscribe(subject);
+        observer3.subscribe(subject);
+        subject.setState("State 1");
+
+        System.out.println(observer1);
+        System.out.println(observer2);
+        System.out.println(observer3);
+
+        subject.setState("State 2");
+
+        System.out.println(observer1);
+        System.out.println(observer2);
+        System.out.println(observer3);
+
+
+        //Prototype
         Pizza pizza = new Pizza("Margharita", 45, true);
         Prototype clonedPizza = pizza.makeClone();
 
